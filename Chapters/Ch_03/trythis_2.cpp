57c45a4a1fa35e9d3527011912938bd1a28c71bb
AllyOMara <allystellaomara@gmail.com>
2025-11-15 17:43:47 +1100

    Update formatting
    Align outputs for easier reading.

--- Chapters/Ch_03/trythis_2.cpp
+++ Chapters/Ch_03/trythis_2.cpp
@@ -8,12 +8,12 @@ int main()
     int n;
     cin >> n;
     double nDouble = n;
-    cout << "n == " << n
-    << "\nn+1 == " << n+1
-    << "\nthree times n == " << 3*n
-    << "\ntwice n == " << n+n
-    << "\nn squared == " << n*n
-    << "\nhalf of n == " << nDouble/2
+    cout << "n == "             << n
+    << "\nn+1 == "              << n+1
+    << "\nthree times n == "    << 3*n
+    << "\ntwice n == "          << n+n
+    << "\nn squared == "        << n*n
+    << "\nhalf of n == "        << nDouble/2
     << "\nsquare root of n == " << sqrt(nDouble)
     << '\n'; // another name for newline (“end of line”) in output
     keep_window_open();
